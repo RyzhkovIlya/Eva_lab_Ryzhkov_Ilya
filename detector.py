3e34fc051e9e96679f0eaf0785b6f061ea9ca15a
Ilya Ryzhkov <ryzhkov.ilya17@gmail.com>
2021-12-03 17:30:41 +0300

    fix

--- detector.py
+++ detector.py
@@ -5,23 +5,23 @@ import argparse
 PH = 'C:/Sigarette/darknet-master/' # Необходимо указать путь до папки darknet-master
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--source")
-args = parser.parse_args()
-imput_file = args.source
-path = f"image/{imput_file}"
+parser.add_argument("--source") # Добавляем кастомный флаг ввода "--source"
+args = parser.parse_args() # ввод в командную строку. Пример - "python3 detector.py --source 00001.png"
+imput_file = args.source # Введенное изображение "00001.png"
+path = f"image/{imput_file}" # Изображение из папки "image"
 
 def result_out(photo, im_file):
     '''
     Функция принимает фотографию и возвращает фотографию с отрисованными bounding box сигареты
     '''
 
-    if photo[-3:] == "mp4":
+    if photo[-3:] == "mp4": # Если файл - видео
         os.system(f"{PH}darknet.exe detector demo {PH}cfg/coco.data {PH}cfg/custom-yolov4-detector_sig.cfg {PH}custom-yolov4-detector_best_sig.weights {photo}")
-    else:
+    else: # Если файл - фотграфия
         os.system(f"{PH}darknet.exe detector test  {PH}cfg/coco.data {PH}cfg/custom-yolov4-detector_sig.cfg {PH}custom-yolov4-detector_best_sig.weights {photo} -dont-show")
-        if os.path.exists(f"{im_file[:5]}_out.{im_file[6:]}"):
+        if os.path.exists(f"{im_file[:5]}_out.{im_file[6:]}"): # Если делаем предсказание второй раз на одной и той де фотографии, то перезаписываем ее
             os.remove('predictions.jpg') # Удаляем файл "predictio.jpg"
         else:
-            os.rename("predictions.jpg", f"{im_file[:5]}_out.{im_file[6:]}") # Переименовываем файл в формат "0001_out.jpg"
+            os.rename("predictions.jpg", f"{im_file[:5]}_out.{im_file[6:]}") # Переименовываем файл в формат "0001_out.png"
 
-result_out(path, imput_file)
+result_out(path, imput_file) # Делаем предсказания всех файлов из папки "image" 
